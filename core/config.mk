ac354cd3887e80e78dccc89df2191edda8ab7cb7
zadli <00vopoj00@gmail.com>
2021-08-22 18:06:59 +0000

    build: bubblegummed

--- core/config.mk
+++ core/config.mk
@@ -295,7 +295,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 ifneq ($(CUSTOM_BUILD),)
-include vendor/aosp/config/BoardConfig.mk
+include vendor/bubblegum/config/BoardConfig.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -446,9 +446,9 @@ ifeq (,$(strip $(PDK_FUSION_PLATFORM_ZIP)$(PDK_FUSION_PLATFORM_DIR)))
   _pdk_fusion_search_paths := \
     vendor/pdk/$(TARGET_DEVICE)/$(TARGET_DEVICE)-$(TARGET_BUILD_VARIANT)/platform \
     vendor/pdk/$(TARGET_DEVICE)/$(TARGET_PRODUCT)-$(TARGET_BUILD_VARIANT)/platform \
-    vendor/pdk/$(TARGET_DEVICE)/$(patsubst aosp_%,full_%,$(TARGET_PRODUCT))-$(TARGET_BUILD_VARIANT)/platform \
+    vendor/pdk/$(TARGET_DEVICE)/$(patsubst bubblegum_%,full_%,$(TARGET_PRODUCT))-$(TARGET_BUILD_VARIANT)/platform \
     vendor/pdk/$(TARGET_PRODUCT)/$(TARGET_PRODUCT)-$(TARGET_BUILD_VARIANT)/platform \
-    vendor/pdk/$(TARGET_PRODUCT)/$(patsubst aosp_%,full_%,$(TARGET_PRODUCT))-$(TARGET_BUILD_VARIANT)/platform
+    vendor/pdk/$(TARGET_PRODUCT)/$(patsubst bubblegum_%,full_%,$(TARGET_PRODUCT))-$(TARGET_BUILD_VARIANT)/platform
 
   _pdk_fusion_default_platform_zip := $(strip $(foreach p,$(_pdk_fusion_search_paths),$(wildcard $(p)/platform.zip)))
   ifneq (,$(_pdk_fusion_default_platform_zip))
